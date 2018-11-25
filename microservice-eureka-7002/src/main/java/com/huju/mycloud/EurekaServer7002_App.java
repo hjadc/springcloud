7a041401a469f74edbaa21f97fff71d8637eb977
huju <13886338732@163.com>
2018-11-25 20:17:13 +0800

    eureka集群搭建(记得修改hosts映射)

---
+++ microservice-eureka-7002/src/main/java/com/huju/mycloud/EurekaServer7002_App.java
@@ -0,0 +1,18 @@
+package com.huju.mycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka注册中心
+ * Created by huju on 2018/11/25.
+ */
+@EnableEurekaServer     // 开启Eureka服务 ,接受其他微服务注册进来
+@SpringBootApplication
+public class EurekaServer7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+}
