97aa05bba38d9cdff6b3fe242a516dd29800fa73
huju <13886338732@163.com>
2018-12-02 20:27:44 +0800

    修改Ribbon的分配策略

--- README.md
+++ README.md
@@ -1,27 +1,8 @@
 #spring cloud练习
 
-###1.整合swagger-ui
-###2.整合redis
-    修改redis的默认序列化为json(默认为jdk)
-    练习各种存储方式使用的方法
-###3.读取配置文件的值和参数校验的使用
-    注入自定义的bean
-    读取多配置文件
-    练习yml文件数据的各种写法
-###4.slf4日志
-
-###5.crud练习
-    整合mybatis
-    配置Druid数据源与Druid监控
-    thymeleaf模板的使用
-    国际化
-    配置默默人首页
-    spring mvc的权限拦截
-    抽取公用的菜单列表
-    完成增删改查
-   
-###6.定制异常页面
-###7.注册servlet三大件
+###1.Ribbon
+    Ribbon是基于客户端的负载均衡
+    com/huju/mycloud/config/ConfigBean.java:17 开启负载均衡与配置分配策略
 
 ### 项目说明
     1.microservice-api  -- 这是一个公用的包,目前就定义了一个公用的实体类
--- microservice-consumer-dept-80/src/main/java/com/huju/mycloud/config/ConfigBean.java
+++ microservice-consumer-dept-80/src/main/java/com/huju/mycloud/config/ConfigBean.java
@@ -1,14 +1,18 @@
 package com.huju.mycloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * Ribbon的负载均衡策略有七个,默认是轮询的
  * Created by huju on 2018/11/25.
  */
-
 @Configuration
 public class ConfigBean {
 
@@ -17,5 +21,19 @@ public class ConfigBean {
     public RestTemplate getRestTemplate(){
 
         return new RestTemplate();
+    }
+
+    /**
+     * 使用随机分配的负载均衡策略替换掉轮询
+     * 需要什么策略就new 什么算法
+     * @return
+     */
+    @Bean
+    public IRule myRule(){
+
+        // new RoundRobinRule();   轮询
+        // new RandomRule();       随机
+        // new RetryRule();        重试机制,(默认轮询,当遇到一个服务多次连接失败后自动跳过该服务继续轮询别的服务)
+        return new RandomRule();
     }
 }
